9d3e046f46f5c756ea4d062abf0f5c9e11d80903
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-06-01 10:54:11 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,15 @@
 'use strict';
+
+function favIceCream(icecream) {
+  return "I love " + icecream
+}
+
+function shouting(string) {
+  return string.toUpperCase()
+}
+function roundDown(num) {
+  return Math.floor(num)
+}
+function theTruth() {
+  return true
+}
